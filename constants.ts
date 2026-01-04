bcb674b1150851ad6344dfc8b7bc768480e21bb3
THEARTDANCESTUDIO <deeart1120@gmail.com>
2026-01-04 23:23:04 +0900

    feat: Add initial MINHEE dancer profile
    Includes MINHEE's dancer profile with an ID, name, role, and image URL to the initial list of dancers.

--- constants.ts
+++ constants.ts
@@ -2,5 +2,16 @@
 import { Dancer } from './types';
 
 export const INITIAL_DANCERS: Dancer[] = [
-  { id: '1', name: 'GAYOON', role: 'DANCER', imageUrl: 'https://lh3.googleusercontent.com/d/1-EVDP8Xt7nFoTxaTlqh2H3DbZs4dNpcG' },
+  { 
+    id: '1', 
+    name: 'GAYOON', 
+    role: 'DANCER', 
+    imageUrl: 'https://lh3.googleusercontent.com/d/1-EVDP8Xt7nFoTxaTlqh2H3DbZs4dNpcG' 
+  },
+  { 
+    id: '2', 
+    name: 'MINHEE', 
+    role: 'DANCER', 
+    imageUrl: 'https://lh3.googleusercontent.com/d/1VgvUNNp8Cm3TMgZ3P6MJbiEIvTiva5m8=w1000?authuser=0$0' 
+  },
 ];
